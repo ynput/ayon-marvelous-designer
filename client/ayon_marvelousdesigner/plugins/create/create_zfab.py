750851b401782fc63758ed4a6f385fc41dcaa4e2
Kayla Man <moonyuet2008@hotmail.com>
2025-12-30 14:41:21 +0800

    use the first fabric if users do not select any zfab

--- client/ayon_marvelousdesigner/plugins/create/create_zfab.py
+++ client/ayon_marvelousdesigner/plugins/create/create_zfab.py
@@ -31,6 +31,8 @@ class CreateZFab(plugin.MDCreator):
         """
         if pre_create_data.get("use_selection"):
             instance_data["fabricIndex"] = fabric_api.GetCurrentFabricIndex()
+        else:
+            instance_data["fabricIndex"] = 1
 
         instance = self.create_instance_in_context(product_name,
                                                    instance_data)
@@ -43,7 +45,7 @@ class CreateZFab(plugin.MDCreator):
     @staticmethod
     def get_pre_create_attr_defs() -> list:
         """Get pre-creation attribute definitions.
-        
+
         Returns:
             list: List of attribute definitions for pre-creation configuration.
         """
